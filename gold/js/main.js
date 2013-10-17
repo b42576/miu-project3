535dcaee2a35d5853a45bd90d6ecc6b62a60e882
Kevin Fry <kevinfry@fullsail.edu>
2013-10-17 14:43:02 -0400

    started with displaying data
    could not get a jquery dialog box popup to appear when successful
    completion of the form, but i'll come back to it.

--- gold/js/main.js
+++ gold/js/main.js
@@ -1,308 +1,114 @@
-// Kevin Fry - MIU 1310 - Project 3
-// must fix editing an item, currently duplicates them
+var autofillData = function (){
+	 
+};
 
-window.addEventListener("DOMContentLoaded", function () {
-    function getEId(x) {
-        var theElement = document.getElementById(x);
-        return theElement;
-    } ;
-    
-    function makeStores() {
-        var formTag = document.getElementsByTagName("form"),
-                selectLi = getEId('locations'),
-                makeSelect = document.createElement('select');
-                makeSelect.setAttribute("id", "location");
-        for(var i=0, j=stores.length; i<j; i++){
-            var makeOption = document.createElement('option');
-            var optText = stores[i];
-            makeOption.setAttribute("value", optText);
-            makeOption.innerHTML = optText;
-            makeSelect.appendChild(makeOption);
-        }
-        selectLi.appendChild(makeSelect);
-    };
-/*
-    function makeTypes(){
-        var formTag = document.getElementsByTagName("form"),
-                selectLi = getEId('sizeTypes'),
-                makeSelect = document.createElement('select');
-                makeSelect.setAttribute("id", "sizeType");
-        for(var i=0, j=sizeTypes.length; i<j; i++){
-            var makeOption = document.createElement('option');
-            var optText = sizeTypes[i];
-            makeOption.setAttribute("value", optText);
-            makeOption.innerHTML = optText;
-            makeSelect.appendChild(makeOption);
-        }
-        selectLi.appendChild(makeSelect);
-    };
-    */
-    // find value of selected radio buttin
-    function getSelectedRadio(){
-        var radios = document.forms[0].tax;
-        for(var i=0; i<radios.length; i++){
-            if (radios[i].checked){
-                taxValue = radios[i].value;
-            }
-        }
-    };
-    /*
-    function getData(){
-        var makeDiv = document.createElement('div');
-        makeDiv.setAttribute("id", "items");
-        var makeList = document.createElement('ul');
-        makeDiv.appendChild(makeList);
-        for(var i = 0, len=localStorage.length; i<len;i++){
-            var makeli = document.createElement('li');
-        }
+var getData = function () {
+    for (var i = 0, len = localStorage.length; i < len; i++) {
+        var key = localStorage.key(i);
+        var value = localStorage.getItem(key);
+        var obj = JSON.parse(value);
+        /*for (var n in obj) {
+            var optSubText = obj[n][0] + " " + obj[n][1];
+            $(".showData").append(optSubText);
+            console.log(optSubText);
+        }*/
+        jQuery.each( obj, function( n, obj ) {
+        $( ".showData" ).append( n + " " + obj.value + "<br />" );
+        });
     }
-    */
-    function toggleControls(n){
-        switch(n){
-            case "on":
-                getEId('pricebookForm').style.display = "none";
-                getEId('clearPricebook').style.display = "inline";
-                getEId('displayPricebook').style.display = "none";
-                getEId('addNew').style.display = "inline";
-            break;
-            case "off":
-                getEId('pricebookForm').style.display = "block";
-                getEId('clearPricebook').style.display = "inline";
-                getEId('displayPricebook').style.display = "inline";
-                getEId('addNew').style.display = "inline";
-                getEId('items').style.display = "none";
-            break;
-            default: return false;
-                
-        }
-    };
-    // not saving to localstorage.
-    // submitting my work to find out why later on the next project
-    function storeData(key){
-        var alertMessage;
-        if(!key){
-            var id = Math.floor(Math.random()*100000001);
-            alertMessage = "Item Added";
-        }else{
-            id=key;
-            alertMessage = "Item Edited";
-        }
-        
-        // get form values // store in objet
-        getSelectedRadio();
-        // object properties contain array with the form label and input values
-        console.log (getEId('location').value);
-        var item = {
-            "location"           : ["Location:", getEId('location').value],
-            "productBrand"       : ["Brand:", getEId('productBrand').value],
-            "productName"        : ["Name:", getEId('productName').value],
-            "description"        : ["Description:", getEId('productDescription').value],
-            "productRating"      : ["Rating:", getEId('productRating').value],
-            "price"              : ["Price:", getEId('price').value],
-            "tax"                : ["Taxable:", taxValue],
-            "datePurchased"      : ["Date:", getEId('datePurchased').value],
-            "size"               : ["Size:", getEId('size').value]
-        };
-            //item.store              = ["Store:", getEId('productBrand').value];
-            //item.sizeType           = ["Size Type:", getEId('sizeType').value];
-            var info=JSON.stringify(item);
-            localStorage.setItem(id, info);
-            alert(alertMessage);
-            //window.location.reload();
-            getData();
-    };
-    
-    function getData(){
-        toggleControls("on");
-        if(localStorage.length === 0){
-            autoFillData();
-            alert("There are no items in your pricebook, so we're adding some for you.");
-        }
-        var makeDiv = document.createElement('div');
-        makeDiv.setAttribute("id", "items");
-        var makeList = document.createElement('ul');
-        makeDiv.appendChild(makeList);
-        document.body.appendChild(makeDiv);
-        getEId('items').style.display = "block";
-        for(var i=0, len=localStorage.length; i<len;i++){
-            var makeLi = document.createElement('li');
-            var linksLi = document.createElement('li');
-            makeList.appendChild(makeLi);
-            var key = localStorage.key(i);
-            var value = localStorage.getItem(key);
-            var obj = JSON.parse(value);
-            var makeSubList = document.createElement('ul');
-            makeLi.appendChild(makeSubList);
-            getImage(obj.location[1], makeSubList);
-            for(var n in obj){
-                var makeSubLi = document.createElement('li');
-                makeSubList.appendChild(makeSubLi);
-                var optSubText = obj[n][0]+" "+obj[n][1];
-                makeSubLi.innerHTML = optSubText;
-                makeSubList.appendChild(linksLi);
-            }
-            makeItemLinks(localStorage.key(i), linksLi);
-        }
-        //makeItemLinks(localStorage.key(i), linksLi);
+};
 
-    };
-    
-    function getImage(storeName, makeSubList){
-        var imageLi = document.createElement('li');
-        makeSubList.appendChild(imageLi);
-        var newImg = document.createElement('img');
-        var setSrc = newImg.setAttribute("src", "img/"+storeName+".png");
-        imageLi.appendChild(newImg);
-    };
-    
-    function autoFillData(){
-        for(var n in json){
-            var id = Math.floor(Math.random()*100000001);
-            localStorage.setItem(id, JSON.stringify(json[n]));
-        }
-    };
-    
-    function makeItemLinks(key, linksLi){
-        var editLink = document.createElement('a');
-        editLink.href = "#";
-        editLink.key = key;
-        var editText = "Edit Item";
-        editLink.addEventListener("click",editItem);
-        editLink.innerHTML = editText;
-        linksLi.appendChild(editLink);
-        
-        var breakTag = document.createElement('br');
-        linksLi.appendChild(breakTag);
-        
-        var deleteLink = document.createElement('a');
-        deleteLink.href = "#";
-        deleteLink.key = key;
-        var deleteText = "Delete Item";
-        deleteLink.addEventListener("click",deleteItem);
-        deleteLink.innerHTML = deleteText;
-        linksLi.appendChild(deleteLink);
-    };
-    function editItem(){
-        var value = localStorage.getItem(this.key);
-        var item = JSON.parse(value);
-        toggleControls("off");
-        getEId('location').value = item.location[1];
-        getEId('productBrand').value = item.productBrand[1];
-        getEId('productName').value = item.productName[1];
-        getEId('productDescription').value = item.description[1];
-        getEId('productRating').value = item.productRating[1];
-        getEId('price').value = item.price[1];
-        //item.tax                = ["Taxable?", taxValue];
-        var radios = document.forms[0].tax;
-        for(var i=0; i<radios.length;i++){
-            if(radios[i].value === "Yes" && item.tax[1] === "Yes"){
-                radios[i].setAttribute("checked","checked");
-            }else if(radios[i].value === "No" && item.tax[1] === "No"){
-                radios[i].setAttribute("checked","checked");
-            }
-        }
-        getEId('datePurchased').value = item.datePurchased[1];
-        getEId('size').value = item.size[1];
-        //getEId('sizeType').value = item.sizeType[1];
-        
-        save.removeEventListener("click", storeData);
-        getEId('submit').value = "Edit Item";
-        var editSubmit = getEId('submit');
-        editSubmit.key = this.key;
-        editSubmit.addEventListener("click", validate);
+var storeData = function (data) {
+    var id = Math.floor(Math.random() * 100000001);
+    var info = JSON.stringify(data);
+    console.log(data);
+    localStorage.setItem(id, info);
+    console.log(id);
+    console.log(info);
+    //$( '.popupFormSaved' ).on();
+    //$(".formbottons").
+    getData();
+    window.location.reload();
+}; 
 
-    };
-    
-    function deleteItem(){
-        var ask = confirm("Are you sure you want to delete this item?");
-        if (ask){
-            localStorage.removeItem(this.key);
-            alert("Item was deleted!");
-            window.location.reload();
-        }else{
-            alert("Item was NOT deleted.");
-        }
-    };
-    function clearLocal(){
-        if(localStorage.length === 0) {
-            alert("There is nothing to clear.");
-        }else{
-            localStorage.clear();
-            window.location.reload();
-            return false;
-        }
-    };
-    function validate(e){
-        var getProductBrand = getEId('productBrand');
-        var getProductName = getEId('productName');
-        var getLocation = getEId('location');
-        //var getPrice = getEId('price');
-        //var getSizeTypes = getEId('sizeType');
-        
-        errMsg.innerHTML = "";
-            getProductBrand.style.border = "1px dashed black";
-            getProductName.style.border = "1px dashed black";
-            getLocation.style.border = "1px dashed black";
-            //getPrice.style.border = "1px dashed black";
-            //getSizeTypes.style.border = "1px dashed black";
-         
-        
-        var errorMessages = [];
-        
-        if(getProductBrand.value === ""){
-            var brandError = "Please enter a product brand.";
-            getProductBrand.style.border = "1px dashed red";
-            errorMessages.push(brandError);
-        }
+var	deleteItem = function (){
+			
+};
+					
+var clearLocal = function(){
+    if(localStorage.length === 0) {
+        alert("There is nothing to clear.");
+    }else{
+        localStorage.clear();
+        window.location.reload();
+        return false;
+    }
+};
 
-        if(getProductName.value === ""){
-            var nameError = "Please enter a product name.";
-            getProductName.style.border = "1px dashed red";
-            errorMessages.push(nameError);
-        }
-        
-        if(getLocation.value === "--Select A Store--"){
-            var locationError = "Please select a store";
-            getLocation.style.border = "1px dashed red";
-            errorMessages.push(locationError);
-        }/*
-        if (! (/^\d*(?:\.\d{0,2})?$/.test(getPrice))) {
-            var getPriceError = "Please enter a valid price";
-            getPrice.style.border = "1px dashed red";
-            errorMessages.push(getPriceError);
-        }
-        if(getSizeTypes.value === "--Select A Size Type--"){
-            var sizeTypeError = "Please select a size";
-            getSizeTypes.style.border = "1px dashed red";
-            errorMessages.push(sizeTypeError);
-        }
-        */
-        
-        if(errorMessages.length > 0){
-            for(var i=0,j=errorMessages.length; i<j;i++){
-                var txt = document.createElement('li');
-                txt.innerHTML = errorMessages[i];
-                errMsg.appendChild(txt);
-            }
-            e.preventDefault();
-            return false;
-        }else{
-            storeData(this.key);
-        }
+$('#display').on('pageinit', function(){
+	//code needed for home page goes here
+    getData();
+    $('#display.clearPricebook').on('click', function(){clearLocal();});
+    //clearLink.addEventListener("click", clearLocal);
 
-    };
-    //var sizeTypes = ["--Select A Size Type--","Count","Each","Pack","Fluid Ounce","Fluid Ounces","Ounce","Ounces","Pound","Pounds"],
-            var stores = ["--Select A Store--","ShopRite","Acme","SuperFresh","Giant","Target","WalMart"],
-            //sizeValue,
-            taxValue,
-            errMsg = getEId('errors');
-    makeStores();
-    //makeTypes();
-    
-    var displayLink = getEId('displayPricebook');
-    displayLink.addEventListener("click", getData);
-    var clearLink = getEId('clearPricebook');
-    clearLink.addEventListener("click", clearLocal);
-    var save = getEId('submit');
-    save.addEventListener("click", validate);
 });
+
+$('#home').on('pageinit', function () {
+
+    var myForm = $('#pricebookForm');
+    myForm.validate({
+        errorElement: "div",
+        errorPlacement: function (error, element) {
+            element.parent().parent().before(error);
+        },
+        rules: {
+            location: { required: true },
+            department: { required: true },
+            productBrand: { required: true },
+            productName: { required: true },
+            productRating: { required: true },
+            datePurchased: { required: true, dateISO: true },
+            price: { required: true, number: true, minlength: 3 }
+        },
+        messages: {
+            location: "You're going to want to remember what store you were in.",
+            department: "This is for better clarification of what part of the store you were in.",
+            productBrand: "Please enter the brand name of the product.",
+            productName: "Take a look at some of the examples of the product names.",
+            productRating: "How likely would you purchase this item again?",
+            datePurchased: " Dates are required because dated prices may have gone up.",
+            price: "Recording the prices will allow you to find the best price."
+        },
+        /*invalidHandler: function (form, validator) {
+        var errors = validator.numberOfInvalids();
+        if (errors) {
+        var message = errors == 1
+        ? 'You missed 1 field. It has been highlighted'
+        : 'You missed ' + errors + ' fields. They have been highlighted';
+        $("#popupFormValuesMissing").appand(message);
+        //$("div.error").show();
+        //popupFormValuesMissing
+        $( "#popupFormValuesMissing" ).popup();
+        } else {
+        $("div.error").hide();
+        }
+        },*/
+        submitHandler: function (form) {
+            //$( '.popupFormSaved' ).on();
+            //$(".popupFormSaved").popup();
+            alert("Pricebook entry added!");
+            var data = myForm.serializeArray();
+
+            storeData(data);
+        }
+    });
+
+
+    //any other code needed for addItem page goes here
+
+});
+
+//The functions below can go inside or outside the pageinit function for the page in which it is needed.
+
+
+
